81031b7c3904c5d5dbb021b56dee744bbad23948
Richard Fairhurst <richard@systemeD.net>
2013-02-24 19:03:34 +0000

    Fix custom index brokenness

--- lib/dm-custom-index.rb
+++ lib/dm-custom-index.rb
@@ -11,8 +11,12 @@ module DataMapper
         def create_index_statement(model, index_name, fields)
           statement=old_create_index_statement(model, index_name, fields)
           index_type=nil
-          fields.each { |field| index_type ||= model.send(field).preferred_index }
+          fields.each do |field|
+            field_class = model.send(field)
+            index_type ||= model.send(field).preferred_index if field_class.respond_to?('preferred_index')
+          end
           statement.gsub!(' ('," USING #{index_type} (") unless index_type.nil?
+          statement
         end # def
       end # module SQL
     end # module DataObjectsAdapter
